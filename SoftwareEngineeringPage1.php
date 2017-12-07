4205b7e489a2fc04968d479c4aac70de124aa22f
KeegoGross <ctgb84@mail.missouri.edu>
2017-12-07 15:33:56 -0600

    Add files via upload

---
+++ SoftwareEngineeringPage1.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Find Local Artist and Shows</title>
+    <style> 
+        input[type=text] {
+        width: 130px;
+        box-sizing: border-box;
+        border: 2px solid #ccc;
+        border-radius: 4px;
+        font-size: 16px;
+        background-color: white;
+        background-image: url('searchicon.png');
+        background-position: center; 
+        background-repeat: no-repeat;
+        padding: 12px 20px 12px 40px;
+        -webkit-transition: width 0.4s ease-in-out;
+        transition: width 0.4s ease-in-out;
+        }
+
+        input[type=text]{
+            width: 50%;
+        }
+        
+        body{
+            background-image: url('https://media.giphy.com/media/nYijNcY8PWWrK/giphy.gif');
+            background-size: cover;
+        }
+        
+        div{
+            margin-top: 25%;
+            margin-left: 33%;
+        }
+        
+        button{
+            padding: 10px; 
+            margin-left: 20px;
+        }
+        
+        
+    </style>
+        
+    </head>
+    <body>
+  
+    <div>
+    <input type="text" name="search" placeholder="Search For Concerts Near You and Local Artists">
+    <button type="button" onclick="location.href = 'http://127.0.0.1:65000/SoftwareEngineeringPage2.html';">Search</button>
+    </div>
+        
+    </body>
+</html>
